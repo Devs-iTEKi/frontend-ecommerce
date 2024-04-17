d705de0c5bc1e71f2d2fbf6a794781335f62e16a
haydeemvm <haydeevelasquezm@gmail.com>
2024-04-16 21:24:26 -0600

    Añadí la tienda

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/partials/Header';
 import Footer from './components/partials/Footer';
+import Shop from './pages/Shop';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './components/router/router';
 import { Horse, Heart, Cube, Alien  } from "@phosphor-icons/react";
