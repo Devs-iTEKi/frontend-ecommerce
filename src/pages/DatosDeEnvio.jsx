feeea5e0ad15813c29fa3f6e0a2b5ff110a98447
AndreScaffini <andrescaffini@gmail.com>
2024-04-20 20:58:38 -0600

    Modificacion de Datos de envio de usuario y  pago check out 2

--- src/pages/DatosDeEnvio.jsx
+++ src/pages/DatosDeEnvio.jsx
@@ -2,78 +2,78 @@ import { CaretLeft,  } from "@phosphor-icons/react";
 const DatosDeEnvio = () => {
   return (
     <section>
-        <div class="container-fluid pt-5">
-            <div class="row justify-content-start">
-                <div class="col-11">
-                    <div class="h-100  w-100 container-fluid d-flex justify-content-start align-items-center">
-                        <a class="fs-1 text-black" href="" style="text-decoration: none; color: inherit;"><i class="ph ph-caret-left"></i></a>
-                        <h1 class="px-3 text-black">Datos de envío</h1>
+        <div className="container-fluid pt-5">
+            <div className="row justify-content-start">
+                <div className="col-11">
+                    <div className="h-100  w-100 container-fluid d-flex justify-content-start align-items-center">
+                        <a className="fs-1 text-black" href="" style="text-decoration: none; color: inherit;"><i className="ph ph-caret-left"></i></a>
+                        <h1 className="px-3 text-black">Datos de envío</h1>
                        
                     </div>
                 </div>
-                <div class="col">
-                    <div class="h-100  w-100 container-fluid d-flex justify-content-end align-items-center">
-                        <a class="fs-1 text-black" href="" style="text-decoration: none; color: inherit;"/>                    <a class="fs-1 text-black " href=""><i class="ph ph-caret-right"></i></a>                      
+                <div className="col">
+                    <div className="h-100  w-100 container-fluid d-flex justify-content-end align-items-center">
+                        <a className="fs-1 text-black" href="" style="text-decoration: none; color: inherit;"/>                    <a className="fs-1 text-black " href=""><i className="ph ph-caret-right"></i></a>                      
                     </div>
                 </div>
                 
             </div>
         </div>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col ">
-                    <div class="container-fluid mt-5 shadow-lg p-3 mb-5 bg-body-tertiary ">
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col ">
+                    <div className="container-fluid mt-5 shadow-lg p-3 mb-5 bg-body-tertiary ">
                         
                         
-                        <div class="m-5 py-3">
-                            <div class="d-flex m-3 py-2">
-                                <div class="form w-50 pe-3 text-body-tertiary">
-                                    <input type="text" class="form-control p-3" id="floatingPassword" placeholder="País"/>
+                        <div className="m-5 py-3">
+                            <div className="d-flex m-3 py-2">
+                                <div className="form w-50 pe-3 text-body-tertiary">
+                                    <input type="text" className="form-control p-3" id="floatingPassword" placeholder="País"/>
                                 </div>
-                                <div class="form w-50 ps-3 text-body-tertiary">
+                                <div className="form w-50 ps-3 text-body-tertiary">
                                   
-                                    <input type="text" class="form-control p-3" id="floatingPassword" placeholder="Departamento"/>
+                                    <input type="text" className="form-control p-3" id="floatingPassword" placeholder="Departamento"/>
                                 </div>
                             </div>
-                            <div class="d-flex m-3 py-2">
-                                <div class="form w-50 pe-3 text-body-tertiary">
+                            <div className="d-flex m-3 py-2">
+                                <div className="form w-50 pe-3 text-body-tertiary">
                                     
-                                    <input type="text" class="form-control p-3" id="floatingPassword" placeholder="Municipio"/>
+                                    <input type="text" className="form-control p-3" id="floatingPassword" placeholder="Municipio"/>
                                 </div>
-                                <div class="form w-50 ps-3 text-body-tertiary">
+                                <div className="form w-50 ps-3 text-body-tertiary">
                                    
-                                    <input type="text" class="form-control p-3" id="floatingPassword" placeholder="Distrito"/>
+                                    <input type="text" className="form-control p-3" id="floatingPassword" placeholder="Distrito"/>
                                 </div>
                             </div>
-                            <div class="form m-3 py-2 text-body-tertiary">
+                            <div className="form m-3 py-2 text-body-tertiary">
                                 
-                                <input type="text" class="form-control p-3"  placeholder="Dirección"/>
+                                <input type="text" className="form-control p-3"  placeholder="Dirección"/>
                             </div>
-                            <div class="form m-3 py-2 text-body-tertiary">
+                            <div className="form m-3 py-2 text-body-tertiary">
                                 
-                                <input type="text" class="form-control p-3"  placeholder="Punto de referencia"/>
+                                <input type="text" className="form-control p-3"  placeholder="Punto de referencia"/>
                             </div>
                         </div>   
                     </div>
                 </div>
-                <div class="col-4">
+                <div className="col-4">
                     
-                   <div class="container-fluid  text-center">
-                        <h4 class="py-5 ">Resumen de la compra</h4>
+                   <div className="container-fluid  text-center">
+                        <h4 className="py-5 ">Resumen de la compra</h4>
                    </div>
-                   <div class="container-fluid py-2 border-bottom">
+                   <div className="container-fluid py-2 border-bottom">
                    </div>
-                   <div class="container-fluid d-flex justify-content-between pt-3">
-                        <h5>Sub total:</h5><span class="px-5"> $350.00</span>
+                   <div className="container-fluid d-flex justify-content-between pt-3">
+                        <h5>Sub total:</h5><span className="px-5"> $350.00</span>
                    </div>
-                   <div class="container-fluid d-flex justify-content-between  pt-3">
-                        <h5 class="">Costo de envío:</h5><span class="px-5"> $00.00</span>
+                   <div className="container-fluid d-flex justify-content-between  pt-3">
+                        <h5 className="">Costo de envío:</h5><span className="px-5"> $00.00</span>
                    </div>
                    <div >
-                   <div class="container-fluid py-2 border-bottom">
+                   <div className="container-fluid py-2 border-bottom">
                    </div>
-                   <div class="container-fluid d-flex justify-content-between align-items-center pt-3">
-                    <h5>Total por la compra:</h5><span class="fs-2 fw-medium text-primary px-5"> $350.00</span>
+                   <div className="container-fluid d-flex justify-content-between align-items-center pt-3">
+                    <h5>Total por la compra:</h5><span className="fs-2 fw-medium text-primary px-5"> $350.00</span>
                </div>
                 </div>
             </div>
--- src/pages/PagoCheckOut2.jsx
+++ src/pages/PagoCheckOut2.jsx
@@ -2,43 +2,43 @@ import { CaretLeft,CaretRight  } from "@phosphor-icons/react";
 const PagoCheckOut2 = () => {
   return (
     <section >
-        <div class="container-fluid pt-5">
-            <div class="row justify-content-start">
-                <div class="col">
-                    <div class="h-100  w-100 container-fluid d-flex justify-content-start align-items-center">
-                        <a class="fs-1 text-black" href="" style="text-decoration: none; color: inherit;"><i class="ph ph-caret-left"></i></a>
-                        <h1 class="px-3 text-black">Método de pago</h1>
+        <div className="container-fluid pt-5">
+            <div className="row justify-content-start">
+                <div className="col">
+                    <div className="h-100  w-100 container-fluid d-flex justify-content-start align-items-center">
+                        <a className="fs-1 text-black" href="" style="text-decoration: none; color: inherit;"><i className="ph ph-caret-left"></i></a>
+                        <h1 className="px-3 text-black">Método de pago</h1>
                     </div>
                 </div>
                 
             </div>
         </div>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col ">
-                    <div class="container-fluid mt-5 shadow-lg p-3 mb-5 bg-body-tertiary ">
-                        <div class="form-check mt-3 fs-4  mb-3">
-                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                            <label class="fw-medium  form-check-label px-4" for="flexCheckDefault">
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col ">
+                    <div className="container-fluid mt-5 shadow-lg p-3 mb-5 bg-body-tertiary ">
+                        <div className="form-check mt-3 fs-4  mb-3">
+                            <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                            <label className="fw-medium  form-check-label px-4" for="flexCheckDefault">
                              Tarjeta de crédito/débito
                             </label>
                         </div>
-                        <div class="border-top">
+                        <div className="border-top">
                         </div>
-                        <div class="form-check mt-3 mb-3   fs-4">
-                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                            <label class="fw-medium  form-check-label px-4" for="flexCheckDefault">
+                        <div className="form-check mt-3 mb-3   fs-4">
+                            <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                            <label className="fw-medium  form-check-label px-4" for="flexCheckDefault">
                              Efectivo
                             </label>
-                            <div class="m-5 p-5">
-                                <div class="form m-3  text-body-tertiary">
-                                    <h5 class="fw-normal">Cantidad con la que pagará</h5>
-                                    <input type="text" class="form-control p-3"  placeholder="$400.00"/>
+                            <div className="m-5 p-5">
+                                <div className="form m-3  text-body-tertiary">
+                                    <h5 className="fw-normal">Cantidad con la que pagará</h5>
+                                    <input type="text" className="form-control p-3"  placeholder="$400.00"/>
                                 </div>
                         
-                                <div class=" mt-4">
-                                    <div class="form m-3 text-body-tertiary">
-                                        <input type="submit" class="fw-semibold form-control bg-primary p-3 text-light"  value="Confirmar"/>
+                                <div className=" mt-4">
+                                    <div className="form m-3 text-body-tertiary">
+                                        <input type="submit" className="fw-semibold form-control bg-primary p-3 text-light"  value="Confirmar"/>
                                     </div>
                                 </div>
                             </div>
@@ -46,24 +46,24 @@ const PagoCheckOut2 = () => {
                           
                     </div>
                 </div>
-                <div class="col-4">
+                <div className="col-4">
                     
-                   <div class="container-fluid  text-center">
-                        <h4 class="py-5 ">Resumen de la compra</h4>
+                   <div className="container-fluid  text-center">
+                        <h4 className="py-5 ">Resumen de la compra</h4>
                    </div>
-                   <div class="container-fluid py-2 border-bottom">
+                   <div className="container-fluid py-2 border-bottom">
                    </div>
-                   <div class="container-fluid d-flex justify-content-between pt-3">
-                        <h5>Sub total:</h5><span class="px-5"> $350.00</span>
+                   <div className="container-fluid d-flex justify-content-between pt-3">
+                        <h5>Sub total:</h5><span className="px-5"> $350.00</span>
                    </div>
-                   <div class="container-fluid d-flex justify-content-between  pt-3">
-                        <h5 class="">Costo de envío:</h5><span class="px-5"> $00.00</span>
+                   <div className="container-fluid d-flex justify-content-between  pt-3">
+                        <h5 className="">Costo de envío:</h5><span className="px-5"> $00.00</span>
                    </div>
                    <div >
-                   <div class="container-fluid py-2 border-bottom">
+                   <div className="container-fluid py-2 border-bottom">
                    </div>
-                   <div class="container-fluid d-flex justify-content-between align-items-center pt-3">
-                    <h5>Total por la compra:</h5><span class="fs-2 fw-medium text-primary px-5"> $350.00</span>
+                   <div className="container-fluid d-flex justify-content-between align-items-center pt-3">
+                    <h5>Total por la compra:</h5><span className="fs-2 fw-medium text-primary px-5"> $350.00</span>
                </div>
                 </div>
             </div>
