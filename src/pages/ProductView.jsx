c8a1ade3ea0c8844a75d98ccb1caeb00ed61b9e0
haydeemvm <haydeevelasquezm@gmail.com>
2024-04-24 11:52:08 -0600

    prueba

--- src/pages/ProductView.jsx
+++ src/pages/ProductView.jsx
@@ -10,11 +10,11 @@ const ProductView = () => {
             <div className="container mt-4">
                 <div className="row align-items-center justify-content-center">
                     {/* Imagen del producto */}
-                    <div className="col-lg-6 col-md-4 mb-4 text-center">
+                    <div className="col-lg-4 col-md-4 mb-4 text-center">
                         <img src="img/products/Auriculares Sony WH-1000XM4.svg" className="img-fluid" alt="Producto" />
                     </div>
                     {/* Detalles del producto */}
-                    <div className="col-lg-6 col-md-8 mb-4">
+                    <div className="col-lg-8 col-md-8 mb-4">
                         <h1 className="fw-bold">Auriculares Sony WH-1000XM4</h1>
                         <div className="d-flex my-2">
                             {/* Lista de estrellas */}
@@ -30,11 +30,11 @@ const ProductView = () => {
                         <p> Inalámbricos con cancelación de ruido y sonido de alta calidad.</p>
 
                         {/* Botones de favorito y carrito */}
-                        <div className="row align-items-center justify-content-center justify-content-md-start"> {/* Control de alineación */}
-                            <div className="col-md-1 mb-3 mb-md-0"> {/* Establece el ancho para dispositivos de tamaño medio */}
-                            <HeartStraight size={32}/>
+                        <div className="row align-items-center justify-content-center justify-content-md-start text-center"> {/* Control de alineación */}
+                            <div className="col-md-2 mb-3 mb-md-0 "> {/* Establece el ancho para dispositivos de tamaño medio */}
+                            <HeartStraight size={32} className="addFavorite"/>
                             </div>
-                            <div className="col-md-7"> {/* Establece el ancho para dispositivos de tamaño medio */}
+                            <div className="col-md-6"> {/* Establece el ancho para dispositivos de tamaño medio */}
                                 <button className="btn btn-primary rounded-pill px-4 w-100">Comprar</button>
                             </div>
                         </div>
